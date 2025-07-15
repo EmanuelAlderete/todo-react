bbce866905540ec471487040641662f65bde6bd8
Emanuel Alderete <emanuel.alderete@jusfy.com.br>
2025-07-15 14:17:06 -0300

    feat: integrate SVG icons and enhance App component layout with new icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import Spinner from "./assets/icons/spinner.svg?react";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import Icon from "./components/icon";
 import Text from "./components/text";
 
 export default function App() {
-  return (
-    <Text as="a" variant="body-md-bold" className="text-pink-dark">
-      BEM VINDO
-    </Text>
-  );
+	return (
+		<div className="grid gap-3">
+			<div className="flex flex-col gap-1">
+				<Text as="a" variant="body-sm-bold" className="text-pink-dark">
+					BEM VINDO
+				</Text>
+				<Text as="a">BEM VINDO</Text>
+				<Text as="a" variant="body-md-bold" className="text-green-dark">
+					BEM VINDO
+				</Text>
+			</div>
+
+			<div className="flex gap-1">
+				<Icon svg={TrashIcon} className="fill-pink-base" />
+        <Icon svg={Spinner} className="fill-pink-base" animate />
+			</div>
+		</div>
+	);
 }
